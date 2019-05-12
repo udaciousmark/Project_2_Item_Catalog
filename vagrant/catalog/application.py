4599c9401c9f80436ded6ba7a5ea8b5a91866e4d
Udacious Mark <udaciousmark@gmail.com>
2019-05-11 22:30:24 -0700

    Display categories on index page

--- vagrant/catalog/application.py
+++ vagrant/catalog/application.py
@@ -1,12 +1,25 @@
 #!/usr/bin/env python2
 
-from flask import Flask
+from flask import Flask, render_template
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
+from database_setup import Base, Category, Item
+
+DB_NAME = 'catalog'
 app = Flask(__name__)
+
+engine = create_engine('postgresql+psycopg2:///' + DB_NAME)
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
 
 @app.route('/')
 def index():
-    return 'Index page'
+    categories = session.query(Category).all()
+    return render_template('index.html', categories=categories)
+
 
 if __name__ == '__main__':
     app.debug = True
