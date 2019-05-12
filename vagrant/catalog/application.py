fe8a4ef7fe10db3132d86a5240daa885f27af07f
Udacious Mark <udaciousmark@gmail.com>
2019-05-12 03:50:02 -0700

    Add page to create a new item

--- vagrant/catalog/application.py
+++ vagrant/catalog/application.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python2
 
-from flask import Flask, render_template
+from flask import Flask, redirect, render_template, request, url_for
 from sqlalchemy import create_engine
 from sqlalchemy import desc
 from sqlalchemy.orm import sessionmaker
@@ -56,6 +56,47 @@ def item(category_name, item_name):
                   .filter_by(name=item_name)\
                   .one()
     return render_template('item.html', item=item)
+
+
+@app.route('/catalog/new_item', methods=['GET', 'POST'])
+def new_item():
+    if request.method == 'POST':
+        category_name = request.form['category_name']
+        item_name=request.form['item_name']
+        item_description=request.form['item_description']
+
+        category = session.query(Category)\
+                          .filter_by(name=category_name)\
+                          .first()
+
+        # If category doesn't exist, then we need to create category & item
+        if category is None:
+            category = Category(name=category_name)
+            session.add(category)
+            session.commit()
+
+            new_item = Item(name=item_name, description=item_description, category_id=category.id)
+            session.add(new_item)
+            session.commit()
+
+            return redirect(url_for('category_items', category_name=category_name))
+
+        item = session.query(Item)\
+                      .filter_by(name=item_name)\
+                      .filter_by(category_id=category.id)\
+                      .first()
+
+        # If item name already exists in this category
+        if item is not None:
+            return redirect(url_for('new_item', category_name=category_name))
+
+        new_item = Item(name=item_name, description=item_description, category_id=category.id)
+        session.add(new_item)
+        session.commit()
+
+        return redirect(url_for('category_items', category_name=category_name))
+    else:
+        return render_template('new_item.html')
 
 
 if __name__ == '__main__':
